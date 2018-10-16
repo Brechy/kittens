e1ea4722c66b3b4ce8672a890c8b99ed763371a3
Brechin <brechin@brechy.net>
2018-10-16 12:09:02 -0700

    Init project and add express framework

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+const kittensRoutes = require('./Routes/kittensRoutes')
+app.use('/kittens', kittensRoutes)
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  const status = err.status || 500
+  res.status(status).json({
+    error: err
+  })
+})
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: {message: 'Not found'}
+  })
+})
+
+const listener = () => console.log('Listening on port ${port}')
+app.listen(port, listener)
+
+module.exports = app
