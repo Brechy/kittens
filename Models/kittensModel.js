42fe2dc66a8432078b54370a059591f1032ea6a1
Brechin <brechin@brechy.net>
2018-10-16 12:46:14 -0700

    added kitten models and laid out more CRUD routes to be built

---
+++ Models/kittensModel.js
@@ -0,0 +1,34 @@
+const uuid = require('uuid/v4') //v4 is random
+const kittens = [{
+  id: uuid(),
+  name: 'Spoopy',
+  age: 'katten'
+}]
+
+function getAllKittens() {
+  return kittens
+}
+
+function create(body) {
+  const errors = []
+  const name = body.name
+
+  let response;
+  if(!name) {
+    errors.push('Please name your kitten')
+    response = { error }
+  } else {
+    cosnt kitten = {
+      id: uuid(),
+      name
+    }
+    kittens.push(kitten)
+    response = kitten
+  }
+  return response
+}
+
+module.exports = {
+  getAllKittens,
+  createKittens
+}
--- Routes/kittensRoutes.js
+++ Routes/kittensRoutes.js
@@ -2,7 +2,12 @@ const express = require('express')
 const router = express.Router()
 const kittroller = require('../Controllers/kittensController.js')
 
-router.get('/', kittroller.getAllKittens)
-router.post('/', kittroller.createKittens)
+router.get('/kittens', kittroller.getAllKittens)
+router.get('/kittens/:id', kittroller.getOneKitten)
+
+router.post('/kittens', kittroller.createKittens)
+router.put('/kittens/:id', kittroller.putOneKitten)
+
+router.delete('/kittens/:id', kittroller.deleteOneKitten)
 
 module.exports = router
